c415b6788d29fb1d63890d16c0901de4cfe494bf
Rojina <rojinabaral28@gmail.com>
2020-11-08 11:40:41 +0545

    use of express-handlebars

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const dotenv=require('dotenv');
 const morgan=require('morgan');
-const router=require('./routes/main');
+const router=require('./routes/index');
 const exphbs=require('express-handlebars')
 const connectDB=require('./config/db');
 
