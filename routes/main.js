7831b5b5814541cea82068ae638fa85f7bf700d5
Rojina <rojinabaral28@gmail.com>
2020-11-08 11:16:28 +0545

    Routes

---
+++ routes/main.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('login')
+})
+
+router.get('/dashboard',(req,res)=>{
+    res.send('dashboard')
+})
+
+router.route('/')
+.get((req,res)=>{
+    res.send('login') 
+})
+
+/*router.route('/')
+.get((req,res)=>{
+    res.send('login') 
+})
+
+router.route('/dashboard')
+.get((req,res)=>{
+    res.send('dashboard') 
+})*/
+
+module.exports=router
--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const morgan=require('morgan');
+const router=require('./routes/main');
+const exphbs=require('express-handlebars')
 const connectDB=require('./config/db');
 
 dotenv.config({path:'./config/config.env'});
 connectDB();
 const app=express();
+
+//logging
+if(process.env.NODE_ENV=='devlopment'){
+    app.use(morgan('dev'))
+}
+
+//handlebars
+app.engine('.hbs', exphbs({defaultLayout:'main',extname:'.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use('/',router);
 
 const PORT=process.env.PORT || 5000
 
