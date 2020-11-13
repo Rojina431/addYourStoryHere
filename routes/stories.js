665d8986bb8ffc411a5a27b6cb6839b88b5584a0
Rojina <rojinabaral28@gmail.com>
2020-11-13 20:47:51 +0545

    Getting public stories

--- routes/stories.js
+++ routes/stories.js
@@ -13,12 +13,28 @@ router.post('/',ensureAuth,async (req,res)=>{
    try{
     req.body.user=req.user.id
     await Story.create(req.body)
-    res.redirect('/dashboard');
+    res.render('/dashboard');
    }catch(e){
        console.log(error);
        res.render('error/500')
    }
+})
 
+//get public story
+router.get('/',ensureAuth,async (req,res)=>{
+    try{
+       const stories=await Story.find({status:"public"})
+       .populate('user')
+       .sort({createdAt:'desc'})
+       .lean()
+
+       res.render('story/index',{
+           stories,
+       });
+    }catch(err){
+        console.log(err)
+        res.redirect('error/500')
+    }
 })
 
 module.exports=router
