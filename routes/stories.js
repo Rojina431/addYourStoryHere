2dca056a50f214816aacc4c58a4f464ca54bcd96
Rojina <rojinabaral28@gmail.com>
2020-11-14 13:39:55 +0545

    Edit story

--- routes/stories.js
+++ routes/stories.js
@@ -12,8 +12,10 @@ router.get('/add',ensureAuth,(req,res)=>{
 router.post('/',ensureAuth,async (req,res)=>{
    try{
     req.body.user=req.user.id
-    await Story.create(req.body)
-    res.render('/dashboard');
+    const stories=await Story.create(req.body)
+    res.render('dashboard',{
+        stories
+    });
    }catch(e){
        console.log(error);
        res.render('error/500')
@@ -34,6 +36,38 @@ router.get('/',ensureAuth,async (req,res)=>{
     }catch(err){
         console.log(err)
         res.redirect('error/500')
+    }
+})
+
+//edit added story
+router.get('/edit/:id',ensureAuth,async(req,res)=>{
+     const story=await Story.findOne({_id:req.params.id}).lean()
+     if(!story){
+         res.render('error/404')
+     }
+     if(story.user!=req.user.id){
+         res.redirect('/stories')
+     }else{
+         res.render('story/edit',{
+             story
+         })
+     }
+})
+
+//update  story
+router.put('/:id',ensureAuth,async(req,res)=>{
+    let story=await Story.findById(req.params.id).lean() 
+    if(!story){
+        res.render('error/404')
+    }
+    if(story.user!=req.user.id){
+        res.redirect('/stories')
+    }else{
+        let story=await Story.findOneAndUpdate({_id:req.params.id},req.body,{
+            new:true,
+            runValidators:true
+        })
+        res.redirect('/dashboard')
     }
 })
 
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const exphbs=require('express-handlebars')
 const connectDB=require('./config/db');
 const passport=require('passport')
 const session = require('express-session')
+const methodOverride=require('method-override')
 const MongoStore=require('connect-mongo')(session);
 dotenv.config({path:'./config/config.env'});
 
@@ -21,6 +22,15 @@ const app=express();
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
+//method override
+app.use(methodOverride(function(req,res){
+  if(req.body && typeof req.body=="object" && "_method" in req.body){
+    var method=req.body._method;
+    delete req.body._method;
+    return method
+  }
+}))
+
 //logging
 if(process.env.NODE_ENV=='devlopment'){
     app.use(morgan('dev'))
@@ -31,13 +41,15 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 //handlebars
-const {formatDate,truncate,stripTags,editIcon}=require('./helper/hbs');
+const {formatDate,truncate,stripTags,editIcon,select}=require('./helper/hbs');
+
 //handlebars
 app.engine('.hbs', exphbs({helpers:{
   formatDate,
   truncate,
   stripTags,
-  editIcon
+  editIcon,
+  select
 },defaultLayout:'main',extname:'.hbs'}));
 app.set('view engine', '.hbs');
 
